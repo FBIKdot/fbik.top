704ce306f1d89c9b6f1d88d3df4516ab1bf929e7
FBIKdot <FBIKdot@users.noreply.github.com>
2026-08-16 03:19:56 +0800

    chore: 调整依赖结构并添加catppuccin主题

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,5 +1,6 @@
 import DefaultTheme from "vitepress/theme";
 import type { Theme } from "vitepress";
+import "@catppuccin/vitepress/theme/mocha/peach.css";
 import Layout from "./Layout.vue";
 
 export default {
